1d3043cdb9fffb3032c6b7927cfc250c88cd5ff9
Kostas <23658992+ksfikas@users.noreply.github.com>
2022-06-06 23:41:10 +0300

    commit 2 for project 1

--- Project 1/alien_invasion.py
+++ Project 1/alien_invasion.py
@@ -1,5 +1,6 @@
 import sys, pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -11,22 +12,27 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height)
         )
+        self.ship = Ship(self)
+    
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
 
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
-    ai =AlienInvasion()
+    ai = AlienInvasion()
     ai.run_game()
     
