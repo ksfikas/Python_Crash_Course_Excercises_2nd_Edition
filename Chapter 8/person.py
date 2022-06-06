30d5db219ff3199a8c014c3e8923dcef11d440ad
Kostas <23658992+ksfikas@users.noreply.github.com>
2022-06-06 10:30:16 +0300

    commit to Chapter 8

---
+++ Chapter 8/person.py
@@ -0,0 +1,9 @@
+def building_person(first_name, last_name, age=None):
+    """Return a dictionary of information about a person."""
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+    return person
+
+musician = building_person('jimmi', 'hendrix', age=27)
+print(musician)
